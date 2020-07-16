a1f32fccabe4c3f53840eefcc485efb174d6a39b
Jerin Jahan <crytekredhat@gmail.com>
2020-07-16 16:37:38 +0600

    Commited By Jerin Jahan (Pricing Page UI Done)

--- src/app/pages/inventory/product/pricing/pricing.component.ts
+++ src/app/pages/inventory/product/pricing/pricing.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 export interface PeriodicElement {
-	name: string;
-	position: string;
-	weight: string;
-	symbol: string;
+	product: string;
+	code: string;
+	note: string;
+	uom: string;
 	ucost: string;
 	tcost: string;
 	details: string;
+	priceRule: string;
+	startDate : string;
+	endDate : string;
 }
   
 const ELEMENT_DATA: PeriodicElement[] = [
-	{position: '', name: '', weight: '', symbol: '', ucost: '', tcost: '', details: ''},
-	{position: '', name: '', weight: '', symbol: '', ucost: '', tcost: '', details: ''},
-	{position: '', name: '', weight: '', symbol: '', ucost: '', tcost: '', details: ''},
-	{position: '', name: '', weight: '', symbol: '', ucost: '', tcost: '', details: ''}
+	{product: 'Shoe Oxford', code: '105002', note: 'Black 8', uom: 'Pair', ucost: '2000', tcost: '2500', details: '2400', priceRule: '2450', startDate: '05/15/20', endDate: '05/15/20'},
+	{product: 'Shoe Oxford', code: '105002', note: 'Black 8', uom: 'Pair', ucost: '2000', tcost: '2500', details: '2400', priceRule: '2450', startDate: '05/15/20', endDate: '05/15/20'},
+	{product: 'Shoe Oxford', code: '105002', note: 'Black 8', uom: 'Pair', ucost: '2000', tcost: '2500', details: '2400', priceRule: '2450', startDate: '05/15/20', endDate: '05/15/20'},
+	{product: 'Shoe Oxford', code: '105002', note: 'Black 8', uom: 'Pair', ucost: '2000', tcost: '2500', details: '2400', priceRule: '2450', startDate: '05/15/20', endDate: '05/15/20'}
 ];
 
 @Component({
@@ -23,6 +26,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 	styleUrls: ['./pricing.component.scss']
 })
 export class PricingComponent implements OnInit {
+	showCalculator = false;
 	formData = {
 		id:0,
 		location: '',
@@ -34,7 +38,7 @@ export class PricingComponent implements OnInit {
 		{id:4,name:'Average Cost'}
 	];
 	unitList = ['Kg','Each','Liter','Bottle','Gram'];
-	displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'ucost', 'tcost', 'details'];
+	displayedColumns: string[] = ['product', 'code', 'note', 'uom', 'ucost', 'tcost', 'details', 'priceRule', 'startDate', 'endDate'];
 	dataSource = ELEMENT_DATA;
 	  
 	constructor(
@@ -43,5 +47,9 @@ export class PricingComponent implements OnInit {
 	}
   
 	ngOnInit() {
+	}
+
+	showPriceCalculator(){
+		this.showCalculator = true;
 	}
 }
